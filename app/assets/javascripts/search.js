cf90c71c82b78df170f0405964f73ea76a42c27a
Kyle Henson <khenson.kh@gmail.com>
2015-06-02 20:49:50 -0600

    Search bar working with AJAX

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -6,16 +6,28 @@
 //     })
 //     var results = $.parseJSON(json.responseText).results;
 //     var movies = $.each(results, function(i, movie){
-//     console.log(movie.title + "-" + movie.id + "-" + movie.overview)} 
+//     console.log(movie.title + "-" + movie.id + "-" + movie.overview)}
 //   })
 // });
+var printMovies = function(input){
+  var movies = [];
+  $.each(input, function(i, movie){
+    movies.push( "<li>" + "<img src='http://image.tmdb.org/t/p/w300" + movie.poster_path + "'/>" + movie.title + "-" + movie.id + "</li>");
+  });
+  $("<ul/>", {
+    "id": "searched_movies",
+    html: movies.join( "" )
+  }).appendTo( "#searched_movies" );
+};
+
 $(function() {
   $( "#search_button" ).click(function() {
     console.log($("#search_box").val());
     var input = $("#search_box").val();
-    var json = $.getJSON("http://api.themoviedb.org/3/search/multi?query=" + input + "&language=en&api_key=171eae1994f298598bb52dd4d2a6d877", function(){
-     })
-    var movieResults = $.parseJSON(json.responseText).results;
-//    console.log(movie_results);
-  });
+    var data = $.getJSON("http://api.themoviedb.org/3/search/multi?query=" + input + "&language=en&api_key=171eae1994f298598bb52dd4d2a6d877", function(){
+    })
+    .then(function(success) {
+      printMovies(success.results)
+    });
+  })
 });
